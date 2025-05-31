7c74911c85b82be8fef14c06569b54d409e5ae85
Nuno <nmamendes163@gmail.com>
2025-05-31 19:28:42 +0100

    a

--- Entrega 2/untitled/src/main/java/projeto/es/views/TicketFrame.java
+++ Entrega 2/untitled/src/main/java/projeto/es/views/TicketFrame.java
@@ -1,9 +1,6 @@
 package projeto.es.views;
 
-import projeto.es.models.Room;
-import projeto.es.models.Session;
-import projeto.es.models.Ticket;
-import projeto.es.models.User;
+import projeto.es.models.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -20,23 +17,11 @@ public class TicketFrame extends JFrame {
         JButton addButton = new JButton("Add Ticket");
         addButton.addActionListener(e -> {
             Room room = new Room(
-                "room1", 
-                "Room 1", 
+                "Room 1",
                 6, 
                 8, 
                 false
             );
-            Session session = new Session(
-                "session1",
-                "movie1",
-                room.getId(),
-                java.time.LocalDateTime.now(),
-                room.getRows(),
-                room.getColumns()
-            );
-            Ticket ticket = new Ticket(1, session, 1, 1);
-            user.addTicket(ticket);
-            ticketListModel.addElement(ticket.toString());
         });
 
         setLayout(new BorderLayout());
